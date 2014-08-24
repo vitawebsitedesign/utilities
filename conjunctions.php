d2857382f1b664f9e38cc101fd3ceaa501f0d699
Michael Nguyen <thinkfast.nguyen@gmail.com>
2014-08-24 13:14:36 +1000

    Update conjunctions.php

--- conjunctions.php
+++ conjunctions.php
@@ -2,6 +2,8 @@
 /*
  * PHP array of conjunctions
  * Can be used with "in_array()" (http://php.net/manual/en/function.in-array.php) for Natural Language Processing tasks
+ * List obtained from "http://grammar.yourdictionary.com/parts-of-speech/conjunctions/conjunctions.html"
+ * For more on conjunctions, read "http://www.oxforddictionaries.com/words/conjunctions"
  */
 $conjunctions = ["and", "but", "or", "nor", "for", "yet", "so", "after", "although", "as", "if", "much", "soon", "though", "only", "when", "then", "just", "lest", "now", "since", "that", "once", "than", "rather", "til", "unless", "until", "when", "whenever", "where", "whereas", "wherever", "which", "while", "who", "whoever", "why", "both", "also", "not", "such"];
 ?>
