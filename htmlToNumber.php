f2c48f6e127517b79feff1e2a367a6b73ce45bc6
Michael Nguyen <thinkfast.nguyen@gmail.com>
2014-08-24 14:09:59 +1000

    Create htmlToNumber.php
    initial commit

---
+++ htmlToNumber.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Removes html and other characters from a string (representing a number, e.g. "<td>4.5%</td>") extracted from html
+ * Arguments: numberUnsanitized = string (representing a number) with html tags, percent sign, and whitespace
+ * Returns: floating-point number
+ */
+function htmlToNumber($numberUnsanitized) {
+  $number = strip_tags($numberUnsanitized);
+	$number = trim($number);
+	$number = preg_replace('/,/', '', $number);
+	$number = preg_replace('/%/', '', $number);
+	$number = floatval($number);
+	return $number;
+}
+	
+?>
