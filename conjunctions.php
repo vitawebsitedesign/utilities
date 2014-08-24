4fd471312ff33385e39ae9352217e9f156a89a79
Michael Nguyen <thinkfast.nguyen@gmail.com>
2014-08-24 13:12:54 +1000

    Create conjunctions.php
    initial commit

---
+++ conjunctions.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * PHP array of conjunctions
+ * Can be used with "in_array()" (http://php.net/manual/en/function.in-array.php) for Natural Language Processing tasks
+ */
+$conjunctions = ["and", "but", "or", "nor", "for", "yet", "so", "after", "although", "as", "if", "much", "soon", "though", "only", "when", "then", "just", "lest", "now", "since", "that", "once", "than", "rather", "til", "unless", "until", "when", "whenever", "where", "whereas", "wherever", "which", "while", "who", "whoever", "why", "both", "also", "not", "such"];
+?>
